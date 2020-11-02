9a35c9924091c1bc44585d43c09fabddb8d2cfd0
=ilf <ilf.mav@gmail.com>
2020-11-02 20:15:44 +0300

    story + capabilities

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,16 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
-const cleanCSS = require('gulp-clean-css');
+const cleanCSS = require('gulp-clean-css')
+const autoPrefix = require('gulp-autoprefixer')
 
 function style () {
     return gulp.src('css/style.scss').
-    pipe(sass()).pipe(cleanCSS()).
-    pipe(gulp.dest('css'));
+    pipe(sass())
+    .pipe(autoPrefix({
+        cascade: false
+    }))
+    .pipe(cleanCSS())
+    .pipe(gulp.dest('css'));
 }
 
 exports.style = style;
